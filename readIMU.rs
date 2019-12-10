183b57c22402db1ca121bc81efbd39cafb0bc290
Pedro <pdevasconcelloso@wpi.edu>
2019-12-09 20:44:51 -0500

    Finish build fixing

--- readIMU.rs
+++ readIMU.rs
@@ -8,13 +8,14 @@ use rppal::i2c::I2c;
 const ADDR_IMU: u16 = 0x68;
 
 // DS3231 register addresses.
-const REG_ACCX: usize = 0x59;
-const REG_ACCY: usize = 0x61;
-const REG_ACCZ: usize = 0x63;
-const REG_TEMP: usize = 0x65;
-const REG_GYROX: usize = 0x67;
-const REG_GYROY: usize = 0x69;
-const REG_GYROZ: usize = 0x71;
+const REG_ACCX: u8 = 73;
+const REG_ACCY: u8 = 75;
+const REG_ACCZ: u8 = 77;
+const REG_TEMP: u8 = 101;
+const REG_GYROX: usize = 103;
+const REG_GYROY: usize = 105;
+const REG_GYROZ: usize = 107;
+const REG_ME: u8 = 165;
 
 // Helper functions to encode and decode binary-coded decimal (BCD) values.
 fn bcd2dec(bcd: u8) -> u8 {
@@ -27,18 +28,18 @@ fn dec2bcd(dec: u8) -> u8 {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut i2c_imu = I2c::new()?;
-	let mut imu_who;
+	let mut imu_who = [0u8;4];
 
     // Set the I2C slave address to the device we're communicating with.
     i2c_imu.set_slave_address(ADDR_IMU)?;
 
 
-	i2c_imu.block_read(0x117, &mut imu_who);
-	println!("Who is it? it is {}", imu_who);
+	i2c_imu.block_read(REG_ME, &mut imu_who)?;
+	println!("Who is it? it is {:?}", imu_who);
 
-    let mut reg_imu = [0u8; 7]; //Each data point for this sensor
+    let mut reg_imu = [0u8; 1]; //Each data point for this sensor
     loop {
-        i2c_imu.block_read(REG_ACCX as u8, &mut reg_imu)?;
+        i2c_imu.block_read(REG_ACCX, &mut reg_imu)?;
 		println!("Data read: {:?}", reg_imu);
         thread::sleep(Duration::from_secs(1));
     }

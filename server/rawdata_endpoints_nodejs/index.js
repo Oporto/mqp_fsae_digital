4b562bd610672b4ff843a7a4e36a30123d666ea0
Pedro de Vasconcellos Oporto <pdevasconcelloso@wpi.edu>
2020-03-04 13:56:39 -0500

    added piped streams

--- server/rawdata_endpoints_nodejs/index.js
+++ server/rawdata_endpoints_nodejs/index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const {Firestore} = require('@google-cloud/firestore');
-
+const stream = require('stream');
 // Create a new client
 const firestore = new Firestore();
 
 const port = process.env.PORT || 8080;
+
+var out_stream = new stream.Readable({
+  read(){}
+});
+
+const fillstream = setTimeout(() => {
+  if(out_stream.readableLength === 0){
+      out_stream.push({empty:0});
+  }
+}, 200);
 
 app.use(bodyParser.json()); // for parsing application/json
 
@@ -21,7 +31,14 @@ app.post('/reading_slice', (req, res) =>{
     console.log(req.body);// req data
     //TODO: parse string into data points
     //TODO: store in datastore
+    let data = {empty:0};
+    out_stream.push(data);
     res.send("ok");
+})
+
+app.get('/processed_slice', (req, res) => {
+  out_stream.on('end', () => res.end());
+  out_stream.pipe(res);
 })
 
 app.listen(port, () => {

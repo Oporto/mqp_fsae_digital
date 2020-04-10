e1c1e1f5f5031ff75ff291b7c859f79d79d05987
Pedro de Vasconcellos Oporto <pdevasconcelloso@wpi.edu>
2020-04-10 13:39:54 -0400

    reading multiple on ads

--- python-client/readAds.py
+++ python-client/readAds.py
@@ -5,16 +5,16 @@ class Ads_reader:
     "Includes driver for reading from an ADS 1115"
     def __init__(self, addr):
         self.adc = ADS1x15.ADS1115(address=addr)
-        self.GAIN = 2/3
+        self.GAIN = 1
 
     #General reads
     def read_single(self,ch):
         rd = self.adc.read_adc(ch,self.GAIN)
-        return (6.144)*rd/32767
+        return (4.096)*rd/32767
     
     def read_diff(self):
-        rd = self.adc.read_adc_difference(self.GAIN)
-        return rd/32767
+        rd = self.adc.read_adc_difference(0,self.GAIN)
+        return (4.096)*rd/32767
     
     #Now, function that vary based on what is installed 
 
@@ -24,7 +24,11 @@ class Ads_reader:
 
 ads = Ads_reader(0x48)
 
+
 print(ads.read_single(0))
 print(ads.read_single(1))
 print(ads.read_thermistor())
 print(ads.read_single(3))
+
+while True:
+    print("Difference between 0 and 1: ", ads.read_diff())

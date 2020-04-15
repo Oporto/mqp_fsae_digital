ce4b30d7be61347f3a20f0022fc1c7f8702d74ef
Pedro de Vasconcellos Oporto <pdevasconcelloso@wpi.edu>
2020-04-15 14:01:01 -0400

    plotting

--- python-client/readAds.py
+++ python-client/readAds.py
@@ -10,8 +10,8 @@ class Ads_reader:
         #Class creates version of adc 
         self.adc = ADS1x15.ADS1115(address=addr)
         #Set internal values for gain and max voltage that are paired
-        self.GAIN = 1
-        self.MAX_V = 4.096
+        self.GAIN = 2/3
+        self.MAX_V = 6.144
 
     #General reads
     #Single read of channel ch: a0-0, a1-1, a2-2, a3-3
@@ -70,7 +70,7 @@ class Ads_reader:
 #create object of class above and give it i2C bus address: 0x48
 ads = Ads_reader(0x48)
 
-tc = ads.compute_reads_tc(30,0.5)
+#tc = ads.compute_reads_tc(30,0.5)
 the = ads.compute_reads_the(30, 0.5)
 
 print(the)
